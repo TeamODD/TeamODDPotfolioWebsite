4040f7e111b37da9afc6625e7fec02c40e29efab
N3RO <jho20455744@gmail.com>
2024-12-30 05:45:14 +0900

    Updates

--- assets/Home-BiwKMoBb.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-CNTHAb13.js";function a(){return e.jsxs("div",{className:"home-container",children:[e.jsx("img",{className:"home-image",src:"images/home/home.jpg",alt:""}),e.jsxs("span",{className:"home-image-text-title",children:["Team ODD",e.jsx("br",{}),e.jsx("span",{className:"home-image-text",children:"한성대학교 중앙 게임 개발 동아리"})]})]})}export{a as default};
